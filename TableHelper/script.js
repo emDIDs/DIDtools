a784df9e703333f1d661c63c7390e1d9895465e9
Julia Noe <julia.noe@greatminds.org>
2025-01-08 14:41:32 -0500

    adds script.js file and adds components to table helper page

---
+++ TableHelper/script.js
@@ -0,0 +1,49 @@
+// Things I want in my helper:
+/**
+ * 
+ * 1. Num rows
+ * 2. Num cols
+ * 3. Column header checkbox
+ * 4. Want in
+ * 
+ * 
+ */
+
+/**
+ * export const columns = [
+  { name: 'Diameter (centimeters)', key: 'diameter', isRowHeader: true },
+  {
+    name: 'Distance Around the Circle (centimeters)',
+    key: 'circumference',
+    isRowHeader: false,
+  },
+  {
+    name: 'Value of the Ratio of the Distance Around the Circle to the Diameter',
+    key: 'ratio',
+    isRowHeader: false,
+  },
+];
+
+export const rows = [
+  [
+    {
+      value: '',
+      editable: true,
+      errorText: '',
+      invalid: false,
+    },
+    {
+      value: '',
+      editable: true,
+      errorText: '',
+      invalid: false,
+    },
+    {
+      value: '',
+      editable: false,
+      errorText: '',
+      invalid: false,
+    },
+  ],
+];
+ */
