f0d5fb9ea4f6c5a4842e588bfe820fd26d2616da
Julia Noe <julia.noe@greatminds.org>
2025-01-09 09:57:33 -0500

    creates col headers

--- TableHelper/script.js
+++ TableHelper/script.js
@@ -5,6 +5,7 @@
  * 2. Num cols
  * 3. Column header checkbox
  * 4. Return .tsx file (pascal case name of applet + data.tsx)
+ * 5. Add multiple inputs (or checkboxes) per input
  *
  *
  */
@@ -16,24 +17,26 @@ function makeTable() {
   const numCols = document.getElementById("numCols").value;
   const hasColHeaders = document.getElementById("colHeaders").checked;
 
-  console.log(
-    "numRows, numCols, hasColHeaders:",
-    numRows,
-    numCols,
-    hasColHeaders
-  );
-
   for (let r = 0; r < numRows; r++) {
     // Inserting a new row
     const x = document.getElementById("myTable").insertRow(r);
     // Looping through columns
     for (let c = 0; c < numCols; c++) {
       // Inserting a new cell at index c in the current row
-      const y = x.insertCell(c);
-      // Setting the inner HTML content of the cell
-      const inputBox = document.createElement("input");
-      inputBox.id = `row${r}Col${c}`;
-      y.appendChild(inputBox);
+
+      // If table headers, add them. Else, create regular cell
+      if (hasColHeaders && r === 0) {
+        const colHeader = document.createElement("th");
+        const inputBox = document.createElement("input");
+        inputBox.id = `row${r}Col${c}`;
+        colHeader.appendChild(inputBox);
+        x.appendChild(colHeader);
+      } else {
+        const y = x.insertCell(c);
+        const inputBox = document.createElement("input");
+        inputBox.id = `row${r}Col${c}`;
+        y.appendChild(inputBox);
+      }
     }
   }
 }
